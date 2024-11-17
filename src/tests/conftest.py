3a7eff9f4777cbacf3581f50c009205da73a0515
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-11-17 15:46:40 +0300

    Connect async httpx client

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,24 +2,22 @@ import asyncio
 import os
 import pytest
 
+from typing import AsyncGenerator
 from pathlib import Path
 
+import pytest_asyncio
+from fastapi.testclient import TestClient
+from httpx import AsyncClient, ASGITransport
+
 from src.database import clear_tables
+from src.main import app
 from config_data.config import Config, load_config
 
 config: Config = load_config(".env")
 
 
-@pytest.fixture(scope="session")
-def event_loop():
-    policy = asyncio.get_event_loop_policy()
-    loop = policy.new_event_loop()
-    yield loop
-    loop.close()
-
-
 @pytest.fixture(scope="session", autouse=True)
-def setup_db():
+def client():
     assert config.variablesData.MODE == "TEST"
 
     project_root = Path(__file__)
@@ -31,3 +29,10 @@ def setup_db():
     os.chdir(project_root)
     os.system("alembic upgrade head")
     asyncio.run(clear_tables())
+
+
+@pytest_asyncio.fixture(scope="session")
+async def client() -> AsyncGenerator[TestClient, None]:
+    httpx_client = AsyncClient(transport=ASGITransport(app=app), base_url="http://localhost:8000")
+    async with httpx_client as client:
+        yield client
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,2 +1,11 @@
-def test_main():
-    assert 1 == 1
+import pytest
+from httpx import AsyncClient
+
+from src.tests.conftest import client
+
+
+@pytest.mark.asyncio
+async def test_ping(client: AsyncClient):
+    response = await client.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == "pong"
