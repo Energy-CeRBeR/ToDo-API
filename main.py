2d378359bbc4bcd7c70cb8f3c8512006d0f1ce25
Igor Chesnykh <igor.chesnyx@mail.ru>
2024-12-02 22:10:03 +0300

    Add middleware

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import uvicorn
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from src.users.user_routers import router as users_router
@@ -21,6 +21,21 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="ToDo-API",
     lifespan=lifespan
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
