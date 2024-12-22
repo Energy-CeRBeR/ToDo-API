dcba47e2fad2dc9b5dadda75e9643d566c10d218
root <root@vm724913.vps.masterhost.tech>
2024-12-22 23:54:39 +0300

    changes in main

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ app = FastAPI(
 )
 
 origins = [
+    "https://energy-cerber.ru",
+    "https://api.energy-cerber.ru",
     "http://localhost",
     "http://localhost:3000",
     "http://127.0.0.1",
